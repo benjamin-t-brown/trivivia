e07f97e52ebfb398c4e9e47c4fb81883ece71516
maxsdl <maxsdl-revirtualis@outlook.com>
2024-11-13 21:16:52 -0700

    quiz today

--- test-browser/gwd.test.js
+++ test-browser/gwd.test.js
@@ -6,7 +6,10 @@ const SCRATCH = path.resolve(__dirname, '../', 'test-results-output');
 const PLAYER_CODE = '008421';
 
 // npx playwright install
+// npx
 // npx playwright test test-browser/gwd.test.js --reporter=list
+
+const url = 'https://b.play.geekswhodrink.com/livegame/098/play/?access=d7d0b993-937e-4e68-9b7f-b58ba3369aff'
 
 let ctr = 0;
 async function takeScreenshot(prefix, page) {
@@ -14,26 +17,27 @@ async function takeScreenshot(prefix, page) {
 }
 
 test('GWD Join Quiz', async ({ page }) => {
-  await page.goto('https://play.geekswhodrink.com/');
-  const prefix = 'gwd-join';
-  await takeScreenshot(prefix, page);
-  await page.locator('#playerCode').fill(PLAYER_CODE);
-  await takeScreenshot(prefix, page);
-  await Promise.all([
-    page.waitForResponse(resp => {
-      return (
-        resp.url().includes('api.gorisio.com/clients') && resp.status() === 200
-      );
-    }),
-    page.locator('[type=submit]').click(),
-  ],);
+  // await page.goto('https://play.geekswhodrink.com/');
+  await page.goto(url);
+  const prefix = 'gwd-7_';
+  // await takeScreenshot(prefix, page);
+  // await page.locator('#playerCode').fill(PLAYER_CODE);
+  // await takeScreenshot(prefix, page);
+  // await Promise.all([
+  //   page.waitForResponse(resp => {
+  //     return (
+  //       resp.url().includes('api.gorisio.com/clients') && resp.status() === 200
+  //     );
+  //   }),
+  //   page.locator('[type=submit]').click(),
+  // ],);
   await takeScreenshot(prefix, page);
 
   await new Promise(resolve => setTimeout(resolve, 1000));
 
   await page.getByText('Just Watch').click();
 
-  // await new Promise(resolve => setTimeout(resolve, 1000));
+  await new Promise(resolve => setTimeout(resolve, 500));
 
   await takeScreenshot(prefix, page);
 
